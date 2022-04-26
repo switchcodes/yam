f035d044b33d4782cfec1e4b9d9fe8caaca5bb77
Florian Johann Hafner <58081895+f2hafner@users.noreply.github.com>
2022-04-26 19:55:56 +0200

    switched from svelte-kit to vanilla-ts project for client (#33)
    * switched from svelte-kit to vanilla-ts project for client
    
    * changed style.css
    
    * Update sonar-project.properties

---
+++ client/src/main.ts
@@ -0,0 +1,6 @@
+import { GameEngine } from "$scripts";
+
+const screen = document.getElementById("app");
+const ge = new GameEngine(screen);
+console.log(ge);
+// const app = document.querySelector<HTMLDivElement>("#app");
